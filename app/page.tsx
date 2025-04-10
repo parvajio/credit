a4a818138257a45e135ff91aa0008d1582067a87
parvajio <mdparvajmosharof17@gmail.com>
2025-04-10 19:32:51 +0600

    shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Welcome to Credit</h1>
-        <p>
+    <main className="max-w-7xl mx-auto ">
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-3">
+        <h1 className="text-2xl font-bold">Welcome to Credit</h1>
+        <p className="text-gray-700 text-lg">
           This is a simple application that allows you to transection credit.
         </p>
-        <p>
-          For $25 credit Bonus Sign Up/Sign in Now!!
+        <p className="">
+          For $25 credit Bonus Sign in Now!!
         </p>
-        <button>
-          sign in
-        </button>
+        <Button>
+          <Link href={"/signin"}>Sign In</Link>
+        </Button>
       </div>
     </main>
   );
